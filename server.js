25ba10628c4d88e9a3a2b42cb90910bcd33370f4
Ederson Torresini <boidacarapreta@gmail.com>
2019-11-12 18:29:35 +0000

    Gerador de labirinto em JS

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use("/", express.static(__dirname));
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
